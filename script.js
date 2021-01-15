ea7c359af5fdba32533da1ddcdc7daa30fc51ca7
Emil Sadek <esadek@hotmail.com>
2021-01-14 21:44:27 -0800

    Add: variables for html elements

---
+++ script.js
@@ -0,0 +1,4 @@
+var deaths = document.getElementsByClassName('deaths')
+var deathsRepresented = document.getElementById('deaths-represented')
+var numRows = document.getElementById('num-rows')
+var rows = document.getElementById('rows')
